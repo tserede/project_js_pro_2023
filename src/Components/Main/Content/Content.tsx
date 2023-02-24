0f10c28d95773a021279e89b84bedf02da95894d
tserede <kasy.06@mail.ru>
2023-02-25 02:30:06 +0300

    изменения

--- src/Components/Main/Content/Content.tsx
+++ src/Components/Main/Content/Content.tsx
@@ -1,7 +1,5 @@
 import { CounterComponent } from 'Components/CounterComponent';
-import { routes } from 'Constants/routes';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import style from './Content.module.scss';
 import { ProductList } from './ProductList';
 import { type ProductListType } from './ProductList/type';
@@ -17,19 +15,7 @@ export const Content = ({ ProductListData }: ProductListPropsType) => {
         {ProductList.map((el) => (
           <CounterComponent key={el.id} image={el.image} title={el.title} price={el.price} />
         ))}
-        {/* <CounterComponent image={'./IMG_6404_800x800.jpeg'} title={'Гортензия белая'} price={2890} />
-      <CounterComponent image={'./diantus-evcalipt.jpeg'} title={'Диантусы в коробке c эвкалиптом'} price={3790} />
-      <CounterComponent image={'./krupnie-rozy-prem.jpeg'} title={'Букет крупных роз премиум «Candlelight»'} price={4590}/>
-      <CounterComponent image={'./IMG_6404_800x800.jpeg'} title={'Гортензия белая'} price={2890} />
-      <CounterComponent image={'./diantus-evcalipt.jpeg'} title={'Диантусы в коробке c эвкалиптом'} price={3790} />
-      <CounterComponent image={'./krupnie-rozy-prem.jpeg'} title={'Букет крупных роз премиум «Candlelight»'} price={4590}/>
-      <CounterComponent image={'./IMG_6404_800x800.jpeg'} title={'Гортензия белая'} price={2890} />
-      <CounterComponent image={'./diantus-evcalipt.jpeg'} title={'Диантусы в коробке c эвкалиптом'} price={3790} />
-      <CounterComponent image={'./krupnie-rozy-prem.jpeg'} title={'Букет крупных роз премиум «Candlelight»'} price={4590}/><CounterComponent image={'./IMG_6404_800x800.jpeg'} title={'Гортензия белая'} price={2890} />
-      <CounterComponent image={'./diantus-evcalipt.jpeg'} title={'Диантусы в коробке c эвкалиптом'} price={3790} />
-      <CounterComponent image={'./krupnie-rozy-prem.jpeg'} title={'Букет крупных роз премиум «Candlelight»'} price={4590}/> */}
       </div>
-      <Link to={routes.delivery}>go to</Link>
     </main>
   );
 };
